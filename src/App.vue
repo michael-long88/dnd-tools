062138a242efe85fe1ed238ac6252c85aa23fa95
Michael Long <michaellong@MacBook-Pro.localdomain>
2020-02-12 21:36:59 -0500

    Fix heroku serve error and create form

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!--    <HelloWorld msg="Welcome to Your Vue.js App" />-->
+    <Calculator />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Calculator from "./components/Calculator";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Calculator
+  },
+  data() {
+    return {
+      regularAleCost: 4,
+      premiumAleCost: 6,
+      dc: 10
+    };
+  },
+  methods: {
+    roll1d20: function f() {
+      return Math.floor(Math.random() * 20) + 1;
+    }
   }
 };
 </script>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="tavern-calculator">
+    <div class="container">
+      <form>
+        <div class="form-group row justify-content-center">
+          <label class="col-form-label" for="regularAleCost">Regular Ale Cost: </label>
+          <input class="form-control col-sm-1" v-model.lazy.number="regularAleCost" id="regularAleCost" />
+        </div>
+        <div class="form-group row justify-content-center">
+          <label class="col-form-label" for="premiumAleCost">Premium Ale Cost: </label>
+          <input class="form-control col-sm-1" v-model.lazy.number="premiumAleCost" id="premiumAleCost" />
+        </div>
+        <div class="form-group row justify-content-center">
+          <label class="col-form-label" for="dc">DC: </label>
+          <input class="form-control col-sm-1" v-model.lazy.number="dc" id="dc" />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calculator",
+  // props: {
+  //   regularAleCost: Number,
+  //   premiumAleCost: Number,
+  //   dc: Number
+  // },
+  data() {
+    return {
+      regularAleCost: 4,
+      premiumAleCost: 6,
+      dc: 10
+    };
+  }
+};
+</script>
+
+<style scoped></style>
