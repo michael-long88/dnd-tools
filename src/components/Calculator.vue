4c021b4972fbad67c84b52c2d48834d38b1ac57d
Michael Long <michaellong@MacBook-Pro.localdomain>
2020-02-13 22:03:21 -0500

    Complete first form

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="tavern-calculator">
     <div class="container">
-      <form>
+      <form @submit.prevent="calculateChanceOfSuccess">
         <div class="form-group row justify-content-center">
           <label class="col-form-label" for="regularAleCost">Regular Ale Cost: </label>
           <input class="form-control col-sm-1" v-model.lazy.number="regularAleCost" id="regularAleCost" />
@@ -14,6 +14,15 @@
           <label class="col-form-label" for="dc">DC: </label>
           <input class="form-control col-sm-1" v-model.lazy.number="dc" id="dc" />
         </div>
+        <div class="form-group row justify-content-center">
+          <label class="col-form-label" for="dc">1d20 Roll: </label>
+          <input class="form-control col-sm-1" v-model="roll1d20" disabled id="roll1d20" />
+        </div>
+        <div class="form-group row justify-content-center">
+          <label class="col-form-label" for="dc">Chance of Success: </label>
+          <input class="form-control col-sm-1" v-model="chanceOfSuccess" disabled id="chanceOfSuccess" />
+        </div>
+        <button type="submit">Calculate</button>
       </form>
     </div>
   </div>
@@ -31,8 +40,23 @@ export default {
     return {
       regularAleCost: 4,
       premiumAleCost: 6,
-      dc: 10
+      dc: 10,
+      roll1d20: 0,
+      chanceOfSuccess: 0
     };
+  },
+  methods: {
+    calculateChanceOfSuccess: function f() {
+      this.roll1d20 = Math.floor(Math.random() * 20) + 1;
+      if (this.roll1d20 < this.dc) {
+        this.chanceOfSuccess = 0;
+      } else {
+        this.chanceOfSuccess = this.roll1d20 + (100 - (this.dc * (this.premiumAleCost - this.regularAleCost)));
+        if (this.chanceOfSuccess > 100) {
+          this.chanceOfSuccess = 100;
+        }
+      }
+    }
   }
 };
 </script>
